059c1ff703bc11da9e846304f55318c0d4671ba6
HogwartsMonkey <lirongavrielevy@gmail.com>
2018-11-04 17:41:17 +0200

    css loader added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import Logo from './assets/logo.png';
+import './styles.css';
+
+
 
 class Welcome extends React.Component {
     render()
             {
-               return <h1>webpack config file</h1>
+
+               return(
+               <div>
+               <h1>webpack config file</h1>
+               <img src={Logo}></img>
+               </div>);
             }
 }
 
