2a6ded0442bbce1c8dc2e6b6bda7c0ab7c8ee440
HogwartsMonkey <lirongavrielevy@gmail.com>
2018-11-05 17:44:46 +0200

    first react component

---
+++ src/brands.js
@@ -0,0 +1,9 @@
+
+import slotsHeavenImage from './assets/slots-heaven.png';
+
+const slotsHeaven = { 
+    image: {slotsHeavenImage},
+    offer: 'Get 100% Up to £100 <br> Get Also 200 Free Spins'
+}
+
+export default slotsHeaven;
--- src/main.js
+++ src/main.js
@@ -6,5 +6,17 @@ import Incoming from './incoming.js';
 import icid from './incoming.js';
 import './styles.css';
 import $ from 'jquery';
+import slotsHeaven from './brands.js';
 
 
+class mainOffer extends React.Component{
+    render(){
+        return (
+        <div className="img-container">
+           <img className="img-responsive" src="{slotsHeaven.image}" /> 
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<mainOffer/>,document.getElementById('root'));
