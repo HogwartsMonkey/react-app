5f13ecc1e190bbfa90f4f2c90a0fdc5eb86c5dea
HogwartsMonkey <lirongavrielevy@gmail.com>
2018-11-24 21:27:45 +0200

    functionality added. should be modified

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
-import $ from 'jquery';
-import brands from './brands.js';
 import {incoming} from './incoming.js';
 import Offer from './offer.js';
 import Scroll from './scroll.js';
 import Reviews from './review.js';
-import { runInThisContext } from 'vm';
+
 
 
 
@@ -15,7 +13,7 @@ import { runInThisContext } from 'vm';
 class Head extends React.Component{
     constructor(props){
         super(props);
-        this.state = {mainOffer: 0, icid: 54, scrollLeft: 200}
+        this.state = {mainOffer: 0, icid: 54, scrollLeft: 0}
         this.changeMainOffer = this.changeMainOffer.bind(this);
         this.handleSrcollRight = this.handleSrcollRight.bind(this);
     }
@@ -34,10 +32,18 @@ class Head extends React.Component{
 
     handleSrcollRight(){
         let e= document.querySelector('.horizontal-scroll');
-        let item= document.querySelector('.horizontal-scroll .item');
-        let scrollX= item.style.width;
-        this.setState({scrollLeft:scrollX +20})
-        e.scrollLeft = this.state.scrollLeft;    
+        let item= document.querySelector('.img-container.scroll-item');
+        let scrollX= item.getBoundingClientRect().width;
+        let menuSize = e.getBoundingClientRect().width;
+        console.log({scrollX})
+        console.log({menuSize});
+        this.setState({scrollLeft:scrollX + this.state.scrollLeft +20})
+        if ( menuSize < this.state.scrollLeft + scrollX){
+            return 0;
+        }
+        else{
+        e.scrollLeft = this.state.scrollLeft;
+        }    
     }
 
     
